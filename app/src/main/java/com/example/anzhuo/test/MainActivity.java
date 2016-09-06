102ad1713dec031cec3166ed0853e69ba23df458
anzhuo <362390701@qq.com>
2016-09-06 10:24:50 +0800

    1

--- app/src/main/java/com/example/anzhuo/test/MainActivity.java
+++ app/src/main/java/com/example/anzhuo/test/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.anzhuo.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
