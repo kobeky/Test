558fd5125041550996524ae378eeb602b6f4263d
anzhuo <362919303@qq.com>
2016-09-06 10:27:40 +0800

    2

--- app/src/main/java/com/example/anzhuo/test/MainActivity.java
+++ app/src/main/java/com/example/anzhuo/test/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.anzhuo.test;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //456789
+        //1512313
     }
 }
